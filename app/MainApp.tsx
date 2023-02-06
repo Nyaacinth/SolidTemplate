f601677e07d8a4dcd264d69a7f846de58b989219
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-02-06 11:19:29 +0800

    Lazy Loading

--- app/MainApp.tsx
+++ app/MainApp.tsx
@@ -6,7 +6,7 @@ export const MainApp: Component = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" component={Home} />
             </Routes>
         </Router>
     )
